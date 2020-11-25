7f8d3aaf683989b92c0505b02d9c5703ecdf4326
yazansorour <yazansorour1@gmail.com>
2020-11-25 10:10:00 +0300

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View , Button , Image} from 'react-native';
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View>
+        <Image source={require('./assets/main.svg')} style={{width: 200, height: 200}}/>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button title="Press Me"/>
+      </View>
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#29C17E',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  
 });
